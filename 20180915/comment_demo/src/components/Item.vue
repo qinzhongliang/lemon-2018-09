d81d76cf2aedaf545a712fff2ecae6ae27d13d85
青子 <2788386852@qq.com>
2018-10-17 16:27:29 +0800

    提交了5天的作业

---
+++ 20180915/comment_demo/src/components/Item.vue
@@ -0,0 +1,56 @@
+<template>
+    <li class="list-group-item">
+        <div class="handle">
+            <a href="javascript:void(0);" @click="deleteItem">删除</a>
+        </div>
+        <p class="user"><span>{{comment.name}}</span><span>说:</span></p>
+        <p class="centence">{{comment.content}}</p>
+    </li>
+</template>
+
+<script>
+    export default {
+        props:{//指定属性名和属性值的类型
+            comment:Object,
+            required:true//必须
+        },
+        methods:{
+            deleteItem(){
+                const{comment}=this;
+                if(window.confirm(`确定删除${comment.name}的评论吗？`)){
+                    this.$emit("deleteItem1");
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+li {
+  transition: .5s;
+  overflow: hidden;
+}
+
+li .handle {
+  width: 40px;
+  border: 1px solid #ccc;
+  background: #fff;
+  position: absolute;
+  right: 10px;
+  top: 1px;
+  text-align: center;
+}
+
+li .handle a {
+  display: block;
+  text-decoration: none;
+}
+
+li .centence {
+  padding: 0px 50px;
+}
+
+li .user {
+  font-size: 22px;
+}
+</style>
---
+++ 20180915/comment_demo/src/components/List.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="col-md-8 content">
+        <h3 class="reply">评论回复：</h3>
+        <h2 style='display: none'>暂无评论，点击左侧添加评论！！！</h2>
+        <ul class="list-group">
+          <Item @deleteItem1="deleteItem2(index)" v-for="(comment,index) in comments" :key="index" :comment="comment"></Item>
+        </ul>
+      </div>
+</template>
+
+<script>
+    import Item from "./Item.vue"
+    export default {
+        //声明接收属性:这个属性就会成为组件对象的属性
+        props:["comments"],//只指定属性名
+        components:{
+            Item
+        },
+        methods:{
+            deleteItem2(index){
+                //第一个参数是父组件中v-on绑定的自定义回调方法，第二个参数为传递的参数
+                this.$emit("deleteItem3",index);
+            }
+        }
+    }
+</script>
+
+<style>
+.content{
+    height: 320px;
+    overflow: auto;
+}
+.content .reply {
+  margin-top: 0px;
+}
+</style>
